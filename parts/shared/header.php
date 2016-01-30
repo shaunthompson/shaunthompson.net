cb24b289f5679e48f7b1de5c67ef6d89551a4ca4
shaunthompson <shaunthompson@Shauns-MacBook-Pro.local>
2016-01-30 00:04:55 -0500

    Header Change

--- parts/shared/header.php
+++ parts/shared/header.php
@@ -1,5 +1,5 @@
 <header>
-	<h1><a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a></h1>
+	<h1><a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a> &mdash; Woohoo!</h1>
 	<?php bloginfo( 'description' ); ?>
 	<?php get_search_form(); ?>
 </header>
